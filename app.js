e8abacf5fe67981687332fa76ee7d1ded132c11a
Emre Çolak <colakkemre@gmail.com>
2022-02-23 12:30:02 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('Function Working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server created in ${port} port...`);
+});
